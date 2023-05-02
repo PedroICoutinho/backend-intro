c3887f38fde698c5557943f073eb6bc4bfdc1e86
Alexandre Lima <alexandreatlima@gmail.com>
2023-05-02 10:14:37 -0300

    review

--- index.js
+++ index.js
@@ -6,16 +6,22 @@ app.use(express.json());
 
 const data = [];
 
-app.get("/", (req, res) => {
-  return res.status(200).json("Oi!");
-});
+// CREATE
 
 app.post("/create", (req, res) => {
   data.push(req.body);
 
+  console.log(data);
+
   return res.status(201).json(data[data.length - 1]);
 });
 
+// READ ALL
+
+app.get("/all", (req, res) => {
+  return res.status(200).json(data);
+});
+
 app.listen(4000, () => {
-  console.log("Conectado a porta 4000");
+  console.log("Conectado a porta 4000.");
 });
